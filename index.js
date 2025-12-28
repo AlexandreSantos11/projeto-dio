ce4432f12e642f171dfef0233f58045f70c20a40
AlexandreSantos11 <alexandrezxp3@gmail.com>
2025-12-28 12:02:57 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-const nome = 'Alex'
-const experiencia = 5445
-let nivel
 
-if (experiencia < 1000) {
-  nivel = 'Ferro'
-} else if (experiencia <= 2000) {
-  nivel = 'Bronze'
-} else if (experiencia <= 5000) {
-  nivel = 'Prata'
-} else if (experiencia <= 7000) {
-  nivel = 'Ouro'
-} else if (experiencia <= 8000) {
-  nivel = 'Platina'
-} else if (experiencia <= 9000) {
-  nivel = 'Ascendente'
-} else if (experiencia <= 10000) {
-  nivel = 'Imortal'
-} else {
-  nivel = 'Radiante'
+
+
+torrar()
+function torrar() {
+  console.log("TOrrando pão")
+  
 }
 
-console.log(`O Herói de nome ${nome} está no nivel de ${nivel}`)
